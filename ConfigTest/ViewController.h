c4c3975b0530cdc43ba4d8af8d0fe04246ff81a3
li bo <libo@lidemacbook-pro.lan>
2017-11-07 15:37:34 +0800

    Initial Commit

---
+++ ConfigTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConfigTest
+//
+//  Created by li bo on 11/7/17.
+//  Copyright © 2017 li bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ConfigTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConfigTest
+//
+//  Created by li bo on 11/7/17.
+//  Copyright © 2017 li bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
